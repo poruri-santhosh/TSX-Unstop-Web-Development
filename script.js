36d3595d316204423f2c379e8bd7e45a8de02d05
santhosh <porurisanthosh674@gmail.com>
2025-05-17 22:12:27 +0530

    Initial commit for TSX Unstop Web Development

---
+++ script.js
@@ -0,0 +1,8 @@
+// Logs a message when the page loads
+console.log("Page Loaded - Hello, TSX Web Developer!");
+
+// Called when button is clicked
+function showMessage() {
+    console.log("Button Clicked!");
+    alert("Welcome to TSX Unstop Web Training!");
+}
